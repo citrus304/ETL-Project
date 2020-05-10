c6ca35d97fe604e33dd1e3a4c8f9c27ae9e7e39b
yusufsenturk34 <60807562+yusufsenturk34@users.noreply.github.com>
2020-05-10 18:31:34 -0400

    Add files via upload

---
+++ players_schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Players (
+ ID INT PRIMARY KEY,
+ Name VARCHAR,
+ Instagram_ID VARCHAR,
+ No_followers INT,
+ No_following INT,
+ Age INT,
+ Nationality VARCHAR,
+ Club VARCHAR,
+ Value INT,
+ Wage INT,
+ Overall INT
+);
+
+SELECT * FROM Players;
